e79750585d09c4dbe0fb2e4f6cdc86be18290034
Mohamed Salah <maxbayne@gmail.com>
2023-02-16 11:38:50 +0200

    add swipeview to support remove feature

---
+++ MauiApplication/Pages/CreatePersonPage.xaml.cs
@@ -0,0 +1,21 @@
+namespace MauiApplication.Pages;
+
+public partial class CreatePersonPage
+{
+	public CreatePersonPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_OnClicked(object sender, EventArgs e)
+    {
+        //var listOfPersons = (IList<string>)lstPersons.ItemsSource;
+        
+        //listOfPersons?.Add(txtFirstName.Text);
+
+        //lstPersons.ItemsSource = listOfPersons;
+    }
+
+
+
+}
